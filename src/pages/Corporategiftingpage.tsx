c4cfecd5bc77b28f59943777217cb65dfe97203e
kunal <konnectwithkunal@gmail.com>
2025-12-31 07:45:01 +0530

    corporate

--- src/pages/Corporategiftingpage.tsx
+++ src/pages/Corporategiftingpage.tsx
@@ -15,6 +15,7 @@ import {
   Package,
   Sparkles
 } from 'lucide-react';
+import HeaderWhite from '@/components/HeaderWhite';
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
@@ -182,7 +183,7 @@ const Corporategiftingpage: React.FC = () => {
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <Header />
+      <HeaderWhite />
       
       {/* Hero Section */}
       <section className="relative pt-20">
